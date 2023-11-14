7cb6bfe8f20f2f24d327c2261fa7326f35e8d7b8
Tiago Luiz <tiagobluiz.97@gmail.com>
2023-11-14 17:11:05 +0000

    Setup from Spring Initlizr

---
+++ src/main/kotlin/com/entity/view/EntityViewApplication.kt
@@ -0,0 +1,11 @@
+package com.entity.view
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EntityViewApplication
+
+fun main(args: Array<String>) {
+	runApplication<EntityViewApplication>(*args)
+}
---
+++ src/test/kotlin/com/entity/view/EntityViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.entity.view
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EntityViewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
